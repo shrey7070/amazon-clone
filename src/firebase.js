ece8160c693ccd559d46ea1f866ae9279f648396
Shrey Soni <shreykumar.soni@tcs.com>
2020-09-09 10:13:11 +0530

    ready to begin

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDgeE6g7SY76XtQcWwglHDQVXbUvkGEy98",
+    authDomain: "amzon-clone-c4ecf.firebaseapp.com",
+    databaseURL: "https://amzon-clone-c4ecf.firebaseio.com",
+    projectId: "amzon-clone-c4ecf",
+    storageBucket: "amzon-clone-c4ecf.appspot.com",
+    messagingSenderId: "915134838829",
+    appId: "1:915134838829:web:601e8cc8ae7bd68ff460a4",
+    measurementId: "G-FSW3F83Q16"
+  };
